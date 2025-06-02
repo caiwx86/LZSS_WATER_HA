f473d05d147269d644182848ea1376ea4da1d888
caiwx86 <caiwenxiu0806@163.com>
2025-06-02 16:20:00 +0800

    更新刷新间隔为8小时

--- custom_components/lzss_water/const.py
+++ custom_components/lzss_water/const.py
@@ -2,7 +2,7 @@
 from datetime import timedelta
 
 DOMAIN = "lzss_water"
-DEFAULT_SCAN_INTERVAL = timedelta(hours=1)
+DEFAULT_SCAN_INTERVAL = timedelta(hours=8)
 CONF_ACCOUNT_NUMBER = "account_number"
 API_BASE_URL = "https://wt.lzss.com/fee/waterfee.aspx"
 
